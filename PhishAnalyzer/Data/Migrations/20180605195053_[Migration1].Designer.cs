8b536173edb398b10258d2ae59e63ef75e2a5f24
Brody Jackson <brodyjackson1@gmail.com>
2018-06-05 14:07:59 -0600

    Initial Commit

--- PhishAnalyzer/Data/Migrations/20180605195053_[Migration1].Designer.cs
+++ PhishAnalyzer/Data/Migrations/20180605195053_[Migration1].Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PhishAnalyzer.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180605195053_[Migration1]")]
+    partial class Migration1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PhishAnalyzer/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhishAnalyzer.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public String sender { get; set; }
+        public String subject { get; set; }
+        public String body { get; set; }
+        public DateTime recieved { get; set; }
+
+
+    }
+}
