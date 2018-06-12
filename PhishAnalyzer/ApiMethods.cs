9667a17d58cee05139eb48a39b06a7ff95f5eaba
Brody Jackson <brodyjackson1@gmail.com>
2018-06-12 15:24:41 -0600

    Added URL parsing, and styled the analysis page. About to add virusTotal nuget package and setup API calls

---
+++ PhishAnalyzer/ApiMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhishAnalyzer
+{
+    public class ApiMethods
+    { 
+        public static string findVirusTotalInfo(string url)
+        {
+            //do something return string with info about the url
+            return ""; 
+        }
+
+        //repeat varius api interaction methods for the other analysis sites with each irl 
+    }
+}
