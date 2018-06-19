e2dbf86e2317abe9d9a2b966542cb7378e5ffdc9
Brody Jackson <brodyjackson1@gmail.com>
2018-06-19 11:54:20 -0600

    Decode function for proofpoint URL's working

--- PhishAnalyzer/ApiMethods.cs
+++ PhishAnalyzer/ApiMethods.cs
@@ -2,17 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VirusTotalNET;
+using VirusTotalNET.Objects;
+using VirusTotalNET.ResponseCodes;
+using VirusTotalNET.Results;
 
 namespace PhishAnalyzer
 {
     public class ApiMethods
-    { 
-        public static string findVirusTotalInfo(string url)
+    {
+        public static async Task<string> findVirusTotalInfoAsync(string url)
         {
+            VirusTotal virusTotal = new VirusTotal("de0a5c113302967ab7d535c84e8a6ce49eacbd2952397c43dc99c13a26df423e");
+            virusTotal.UseTLS = true;
+            UrlReport urlReport = await virusTotal.GetUrlReportAsync(url);
+
+            bool hasUrlBeenScannedBefore = urlReport.ResponseCode == UrlReportResponseCode.Present;
             //do something return string with info about the url
-            return ""; 
+            Console.WriteLine("URL has been scanned before: " + (hasUrlBeenScannedBefore ? "Yes" : "No"));
+
+            //If the url has been scanned before, the results are embedded inside the report.
+            if (hasUrlBeenScannedBefore)
+            {
+                PrintScan(urlReport);
+            }
+            else
+            {
+                UrlScanResult urlResult = await virusTotal.ScanUrlAsync(url);
+                PrintScan(urlResult);
+            }
+            return "";
+            //repeat varius api interaction methods for the other analysis sites with each irl 
+        }
+        private static void PrintScan(UrlScanResult scanResult)
+        {
+            Console.WriteLine("Scan ID: " + scanResult.ScanId);
+            Console.WriteLine("Message: " + scanResult.VerboseMsg);
+            Console.WriteLine();
         }
 
-        //repeat varius api interaction methods for the other analysis sites with each irl 
+        private static void PrintScan(ScanResult scanResult)
+        {
+            Console.WriteLine("Scan ID: " + scanResult.ScanId);
+            Console.WriteLine("Message: " + scanResult.VerboseMsg);
+            Console.WriteLine();
+        }
+
+        private static void PrintScan(FileReport fileReport)
+        {
+            Console.WriteLine("Scan ID: " + fileReport.ScanId);
+            Console.WriteLine("Message: " + fileReport.VerboseMsg);
+
+            if (fileReport.ResponseCode == FileReportResponseCode.Present)
+            {
+                foreach (KeyValuePair<string, ScanEngine> scan in fileReport.Scans)
+                {
+                    Console.WriteLine("{0,-25} Detected: {1}", scan.Key, scan.Value.Detected);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void PrintScan(UrlReport urlReport)
+        {
+            Console.WriteLine("Scan ID: " + urlReport.ScanId);
+            Console.WriteLine("Message: " + urlReport.VerboseMsg);
+
+            if (urlReport.ResponseCode == UrlReportResponseCode.Present)
+            {
+                foreach (KeyValuePair<string, UrlScanEngine> scan in urlReport.Scans)
+                {
+                    Console.WriteLine("{0,-25} Detected: {1}", scan.Key, scan.Value.Detected);
+                }
+            }
+        }
     }
-}
+ }
